2801d908a818a150815b41c1bc243520e9583579
DAWDU207 <DAWDU207@acarballeira.local>
2022-10-03 18:49:15 +0200

    features

--- EjercicioGasolinera/logicaGasolinera.js
+++ EjercicioGasolinera/logicaGasolinera.js
@@ -4,11 +4,56 @@
 var capacidadDeposito = 50000;
 var capacidadSuficiente = true;
 var precioLitro = 1.24;
-
+var cantidadLitrosRepostar;
+var intencionRepostar = false;
+var contadorVehiculos = 0 ;
+var importeMedio = 0;
 
 while(capacidadSuficiente){
-    alert("Bienvenido a la gasolinera A carballeira.\n");
+    alert("Bienvenido a la gasolinera A carballeira.\nQuedan "
+    + capacidadDeposito + " Litros.");
+    if(capacidadDeposito>5000){
+        intencionRepostar = prompt("¿Deseas repostar?\n1- Si.\n2- No");
 
-    
+        //Controlamos cuanto quiere repostar, una vez declarado que quiere hacerlo.
+        if(intencionRepostar == 1){
+            cantidadLitrosRepostar = prompt("¿Cuanto deseas repostar?(En euros)");
+
+            if(cantidadLitrosRepostar <= capacidadDeposito-5000){
+                cantidadLitrosRepostar = repostaVehiculo(cantidadLitrosRepostar);
+                contadorVehiculos++;
+                capacidadDeposito -= cantidadLitrosRepostar;
+                alert("Muy bien, se suministran: " + cantidadLitrosRepostar + " Litros. \nQuedan : "+ parseInt(capacidadDeposito) + " Litros");
+            }
+            else{
+                alert("No quedan tantos litros de gasolina.\n")
+            }
+        }
+        else{
+            alert("De acuerdo, vuelva otro día!");
+        }
+        
+    }
+    else{
+        alert("No queda suficiente gasolina para repostar.")
+    }
+
+    if(capacidadDeposito <= 5000){
+        capacidadSuficiente = false;
+    }
 }
 
+calculosFinDia();
+
+//Función que devuelve los litros que reposta el coche (presupuesto/precioLitro)
+function repostaVehiculo(presupuesto){
+    cantidadLitrosRepostar = presupuesto/precioLitro;
+    return cantidadLitrosRepostar;
+}
+
+function calculosFinDia(){
+    var datosDia = "Los datos diarios son: ";
+    datosDia+contadorVehiculos;
+
+    echo (datosDia);
+}
