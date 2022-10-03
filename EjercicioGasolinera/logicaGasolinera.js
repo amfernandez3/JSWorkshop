09cf4a2ffafb6ee0531de26cfb00504250febeb5
DAWDU207 <DAWDU207@acarballeira.local>
2022-10-03 17:43:46 +0200

    added exercise gasolinera

---
+++ EjercicioGasolinera/logicaGasolinera.js
@@ -0,0 +1,14 @@
+//Ejercicio gasolinera
+
+// Capacidad en L
+var capacidadDeposito = 50000;
+var capacidadSuficiente = true;
+var precioLitro = 1.24;
+
+
+while(capacidadSuficiente){
+    alert("Bienvenido a la gasolinera A carballeira.\n");
+
+    
+}
+
