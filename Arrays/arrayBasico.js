9cd6cf803e23b3fca91eeb16b568a599e1a4f237
DAWDU207 <DAWDU207@acarballeira.local>
2022-10-17 22:09:22 +0200

    new exercises

---
+++ Arrays/arrayBasico.js
@@ -0,0 +1,20 @@
+//Javascript: aprendizaje de estructuras de datos básicas.
+var numeros = new Array(10);
+var mayor = 0;
+alert("Introduce 10 números");
+
+for (let i = 0; i < numeros.length; i++) {
+    numeros[i] = prompt("Introduce un número mayor de 20");
+    if (numeros[i] > 20) {
+        if(numeros[i]>mayor){
+            mayor == numeros[i];
+        }
+    }
+    else {
+        alert("El número debe ser mas alto de 20");
+        i--;
+    }
+
+}
+
+alert("Los números introducidos son: " + numeros+ "Y el mayor es: " + mayor);
--- EjercicioGasolinera/logicaGasolinera.js
+++ EjercicioGasolinera/logicaGasolinera.js
@@ -8,6 +8,8 @@ var cantidadLitrosRepostar;
 var intencionRepostar = false;
 var contadorVehiculos = 0 ;
 var importeMedio = 0;
+var flagSalida = false;
+
 
 while(capacidadSuficiente){
     alert("Bienvenido a la gasolinera A carballeira.\nQuedan "
@@ -18,19 +20,25 @@ while(capacidadSuficiente){
         //Controlamos cuanto quiere repostar, una vez declarado que quiere hacerlo.
         if(intencionRepostar == 1){
             cantidadLitrosRepostar = prompt("¿Cuanto deseas repostar?(En euros)");
-
+            cantidadLitrosRepostar = repostaVehiculo(cantidadLitrosRepostar);
+            
             if(cantidadLitrosRepostar <= capacidadDeposito-5000){
-                cantidadLitrosRepostar = repostaVehiculo(cantidadLitrosRepostar);
-                contadorVehiculos++;
+                
                 capacidadDeposito -= cantidadLitrosRepostar;
                 alert("Muy bien, se suministran: " + cantidadLitrosRepostar + " Litros. \nQuedan : "+ parseInt(capacidadDeposito) + " Litros");
+                contadorVehiculos++;
             }
             else{
-                alert("No quedan tantos litros de gasolina.\n")
+                if(confirm("No quedan tantos litros de gasolina, puedes repostar: "+ (capacidadDeposito - 5000)*1.24 +"Euros de gasolina")){
+                    capacidadDeposito = 5000;
+                    contadorVehiculos++;
+                }
             }
         }
         else{
-            alert("De acuerdo, vuelva otro día!");
+            if(!confirm("De acuerdo, vuelva otro día!")){
+                flagSalida = true;
+            }
         }
         
     }
@@ -38,7 +46,7 @@ while(capacidadSuficiente){
         alert("No queda suficiente gasolina para repostar.")
     }
 
-    if(capacidadDeposito <= 5000){
+    if(capacidadDeposito <= 5000 || flagSalida){
         capacidadSuficiente = false;
     }
 }
@@ -52,8 +60,17 @@ function repostaVehiculo(presupuesto){
 }
 
 function calculosFinDia(){
-    var datosDia = "Los datos diarios son: ";
-    datosDia+contadorVehiculos;
+    var importeMedio = (50000 - capacidadDeposito)*1.24 / contadorVehiculos;
+    var porcentajeExtra;
+    if(capacidadDeposito>5000){
+       porcentajeExtra = ((capacidadDeposito - 5000)*100)/5000;
+    }
+    else if(capacidadDeposito == 5000){
+        porcentajeExtra = 0;
+    }
 
-    echo (datosDia);
+    var datosDia = "Los datos diarios son: "+ contadorVehiculos+ "\n" + "La media de gasto es:" + importeMedio
+    + "\nLos litros restantes son: "+ capacidadDeposito + "\nel porcentaje de litros por encima del mínimo es: " + porcentajeExtra;
+    
+    alert(datosDia);
 }
